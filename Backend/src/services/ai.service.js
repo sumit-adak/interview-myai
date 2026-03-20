3a202b5be7f46db3ea489f994646bcb3eaea6d50
sumit-adak <adaksumit999@gmail.com>
2026-03-20 19:43:33 +0530

    resume download update

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -58,7 +58,9 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 
 async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
