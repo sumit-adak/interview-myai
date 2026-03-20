3c542bfa8b49156d709729129c4a08f49c3a8891
sumit-adak <adaksumit999@gmail.com>
2026-03-20 19:50:03 +0530

    update puppeteer

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -59,10 +59,16 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ],
+        headless: true
     })
     const page = await browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
+    await page.setContent(htmlContent, { waitUntil: "load", timeout: 60000 })
 
     const pdfBuffer = await page.pdf({
         format: "A4", margin: {
